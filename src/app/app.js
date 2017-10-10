88fe94ad580ca96cded409a0301fd173539d9037
tokdaniel <tokdaniel4@gmail.com>
2017-10-10 19:01:19 +0200

    Introduction to using styles in React && Sass

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Main } from 'components';
 
+import 'assets/scss/global.scss';
+
 render(
   <Main/>,
   document.getElementById('react-root')
--- src/app/components/StatelessComponent/StatelessComponent.jsx
+++ src/app/components/StatelessComponent/StatelessComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './StatelessComponent.scss';
+
 const StatelessComponent = props => {
   return (
-    <div>
-      { props.value }
+    <div className="stateless-component">
+      <h1>{ props.value }</h1>
     </div>
   );
 };
